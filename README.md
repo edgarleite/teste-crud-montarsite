b4b08e624002dc58a25055c7a9d60554203827f3
Edgar <edgar@MacBook-Pro-de-Edgar.local>
2019-02-20 08:51:31 -0300

    Editado README e anexados arquivo SQL

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
 # Teste CRUD MontarSite
 
-Após clonar o projeto, acessar a pasta deste e rodar o comando abaixo:
+### Requisitos do sistema
+- PHP 7.2.10
+- MySql 5.7.23
+- Yii Framework 2.0.16
 
+### Instalação
 ```sh
+$ cd /caminho/do/servidor/www
+$ git clone https://github.com/edgarleite/teste-crud-montarsite.git nome-da-pasta
+$ cd nome-da-pasta
 $ composer install
 ```
+
+### Configuração
+Editar arquivo `config/db.php` com os dados de acesso ao banco de dados local.
+
+### Banco de dados
+No banco de dados, rodar script SQL do arquivo `teste-crud-montarsite.sql` anexo ao projeto.
+
+### Testanto a aplicação
+
+Você pode usar seu navegador para acessar o aplicativo Yii instalado com o seguinte URL:
+```sh
+http://localhost/teste-crud-montarsite/web
+```
+
+A pasta raíz da aplicação é `/web`.
---
+++ teste-crud-montarsite.sql
@@ -0,0 +1,44 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Versão 4541
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.7.23)
+# Base de Dados: teste-crud-montarsite
+# Tempo de Geração: 2019-02-20 11:15:57 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump da tabela pessoa
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `pessoa`;
+
+CREATE TABLE `pessoa` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) NOT NULL DEFAULT '',
+  `criado_em` datetime NOT NULL,
+  `atualizado_em` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
